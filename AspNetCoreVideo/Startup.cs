956c57e47a3bc3c83696a7c3bfca1d429a94c0e6
Jeff <jeff.neumann@phoenix.edu>
2017-07-16 15:35:26 -0400

    Chapter 5, Part 1 - complete

--- AspNetCoreVideo/Startup.cs
+++ AspNetCoreVideo/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.Configuration;
 using System.IO;
 using AspNetCoreVideo.Services;
 using Microsoft.AspNetCore.Routing;
+using AspNetCoreVideo.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCoreVideo
 {
@@ -18,9 +20,14 @@ namespace AspNetCoreVideo
     {
         public IConfiguration Configuration { get; set; }
 
-        public Startup()
+        public Startup(IHostingEnvironment env)
         {
-            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true);
+
+            if (env.IsDevelopment())
+                builder.AddUserSecrets<Startup>();
 
             Configuration = builder.Build();
               
@@ -29,8 +36,12 @@ namespace AspNetCoreVideo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var conn = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<VideoDbContext>(options =>
+                options.UseSqlServer(conn));
+
             services.AddMvc();
-            services.AddSingleton<IVideoData, MockVideoData>();
+            services.AddSingleton<IVideoData, SqlVideoData>();
             services.AddSingleton(provider => Configuration);
             services.AddSingleton<ImessageService, ConfigurationMessageService>();
         }
@@ -43,6 +54,7 @@ namespace AspNetCoreVideo
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+              
             }
 
             app.UseDefaultFiles();
