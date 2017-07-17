177c86881c80f91c95232e6a9bf526f100d0b3f8
Jeff Neumann <Jeff.Neumann@phoenix.edu>
2017-07-17 17:51:25 -0400

    Initial migration on machine two.

---
+++ AspNetCoreVideo/Migrations/20170717151505_InitialMachine2.Designer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using AspNetCoreVideo.Data;
+using AspNetCoreVideo.Models;
+
+namespace AspNetCoreVideo.Migrations
+{
+    [DbContext(typeof(VideoDbContext))]
+    [Migration("20170717151505_InitialMachine2")]
+    partial class InitialMachine2
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("AspNetCoreVideo.Entities.Video", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Genre");
+
+                    b.Property<string>("Title")
+                        .IsRequired()
+                        .HasMaxLength(80);
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Videos");
+                });
+        }
+    }
+}
