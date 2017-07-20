d58081d3ebb5b63c4a547f61bba7e6dbdecabbcb
Jeff <jeff.neumann@phoenix.edu>
2017-07-20 13:40:49 -0400

    Chapter six completed.

---
+++ AspNetCoreVideo/ViewComponents/Message.cs
@@ -0,0 +1,26 @@
+﻿using AspNetCoreVideo.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreVideo.ViewComponents
+{
+    public class Message : ViewComponent
+    {
+        private ImessageService _message;
+
+        public Message(ImessageService message)
+        {
+            _message = message;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _message.GetMessage();
+            return View("Default", model);
+        }
+
+    }
+}
